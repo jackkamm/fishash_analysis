75952f3cb05d3af81cb862cf83c6a50655e69bc3
Jack Kamm <jackkamm@gmail.com>
2026-05-29 16:33:21 +0000

    Finish updating AUPRC plots

--- 84_plot_vary_precision.R
+++ 84_plot_vary_precision.R
@@ -175,7 +175,48 @@ compress_prc_df <- function(grouped_lazy_dt, nsteps) {
 }
 
 
-## VARYING NUMBER OF GUIDES SCENARIO
+merge_fishash_assignments_teststats <- function(list_teststats, list_assignments) {
+    names(list_teststats) %>%
+        lapply(
+            function(x) {
+                sim <- list_teststats[[x]]$simulation
+                res <- list_teststats[[x]]$results
+                mask <- assay(sim, 'counts') > 0
+
+                # for backward-compatibility
+                if ('fishash_refit10' %in% names(assays(list_assignments[[x]]$results))) {
+                    fishash_assay <- 'fishash_refit10'
+                } else {
+                    fishash_assay <- 'fishash_refit10_padj0.05'
+                }
+
+                data.frame(
+                    test_stat = assay(res, 'fishash_refit10_padj0.05')[mask],
+                    true_guide = assay(sim, 'ground_truth')[mask],
+                    assigned = assay(
+                        list_assignments[[x]]$results, fishash_assay
+                    )[mask],
+                    count = assay(sim, 'counts')[mask],
+                    sim_label = metadata(sim)$sim_label
+                )
+            }
+        ) %>%
+    rbindlist() %>%
+    as.data.frame() %>%
+    mutate(type=factor(case_when(
+        assigned & true_guide ~ 'TP',
+        assigned & !true_guide ~ 'FP',
+        !assigned & true_guide ~ 'FN',
+        !assigned & !true_guide ~ 'TN',
+        TRUE ~ NA_character_
+    ), levels=c('TN', 'FP', 'FN', 'TP')))
+}
+
+
+## VARYING NUMBER OF GUIDES SCENARIOS
+
+
+# prefixes for paths
 
 prefix_high_varyPrec <- file.path(
     OUTS,
@@ -336,6 +377,50 @@ df_prec_recall_varyNguides_crispat %<>%
               .groups='drop') %>%
     add_precision_recall()
 
+# Read fishash assignments from previous run for log-p-value histogram plot
+
+list_assignments_high <- c(
+    readRDS(file.path(
+        prefix_high_orig,
+        "leq20k",
+        "combined_results.Rds"
+    )),
+    readRDS(file.path(
+        prefix_high_orig,
+        "80k",
+        "combined_results.Rds"
+    ))
+)
+
+list_assignments_low <- c(
+    readRDS(file.path(
+        prefix_low_orig,
+        "leq20k",
+        "combined_results.Rds"
+    )),
+    readRDS(file.path(
+        prefix_low_orig,
+        "80k",
+        "combined_results.Rds"
+    ))
+)
+
+rbind(
+    merge_fishash_assignments_teststats(list_teststats_highexpr, list_assignments_high) %>%
+        mutate(regime='high_grna'),
+    merge_fishash_assignments_teststats(list_teststats_lowexpr, list_assignments_low) %>%
+        mutate(regime='low_grna')
+) ->
+    df_fishash_teststats_varyNguides
+
+df_fishash_teststats_varyNguides %<>%
+    dplyr::select(sim_label) %>%
+    dplyr::distinct() %>%
+    cbind(parse_simlab_varyNumGuides(.$sim_label)) %>%
+    dplyr::right_join(
+        df_fishash_teststats_varyNguides       
+    )
+
 ## Plots
 
 pdf(file.path(plot_dir, "numCells20k_varyNumGuides_varyPrecision_precVsRecall.pdf"),
@@ -355,10 +440,11 @@ bind_rows(
                default=TRUE),
     ) %>%
     mutate(method=forcats::fct_relevel(method, method_levels)) %>%
+    rename(default_threshold=default) %>%
     arrange(desc(curve_type), method) %>% {
         ggplot(., aes(x=Recall, y=Precision, color=method)) +
             geom_step(aes(lty=curve_type), direction="vh") +
-            geom_point(aes(shape=default),
+            geom_point(aes(shape=default_threshold),
                        data=filter(., curve_type=='Discretized')) +
             facet_grid(regime~nguides, labeller='label_both') +
             scale_color_manual(values=method_colors) +
@@ -366,12 +452,9 @@ bind_rows(
             scale_linetype_manual(values=c('solid', 'dotdash')) +
             xlim(0,1) +
             ylim(0,1) +
-            guides(shape=guide_legend(nrow=2), linetype=guide_legend(nrow=2),
-                   color=guide_legend(nrow=2)) +
+            guides(color="none") +
             theme_classic(base_size=16) +
             theme(axis.text.x=element_text(angle=45, hjust=1),
-                  legend.box.margin = margin(t = 0, r = 0, b = 0, l = -30,
-                                             unit = "pt"),
                   legend.position='bottom')
     }
 dev.off()
@@ -392,23 +475,39 @@ pdf(file.path(plot_dir, "numCells20k_varyNumGuides_varyPrecision_auprc_vs_f1.pdf
               select(method, sim_label, regime, nguides, F1)
       ) %>%
       .[sample(1:nrow(.)),] %>%
+      arrange(curve_type) %>%
       ggplot(aes(x=F1, y=AUPRC, color=method, shape=curve_type)) +
       geom_point(aes(size=curve_type)) +
       scale_color_manual(values=method_colors) +
       facet_grid(regime~nguides, labeller='label_both') +
       #xlim(0,1) + ylim(0,1) +
       scale_shape_manual(values=c(1,4)) +
-      scale_size_manual(values=c(2.75,1)) +
+      scale_size_manual(values=c(3,1.5)) +
       theme_bw(base_size=16) +
       theme(legend.position='bottom')
 dev.off()
 
 pdf(file.path(plot_dir, "numCells20k_varyNumGuides_varyPrecision_auprc.pdf"),
     width=14, height=7)
-df_prec_recall_varyNguides_auprc %>%
-    ggplot(aes(x=method, y=AUPRC, color=method)) +
+bind_rows(
+    df_teststats_varyNguides_auprc %>%
+        mutate(curve_type="Full"),
+    df_prec_recall_varyNguides_auprc %>%
+        mutate(curve_type="Discretized")
+) %>%
+    mutate(method2=method) %>%
+    mutate(method=if_else(
+        curve_type == 'Full',
+        sprintf("%s (Full)", method),
+        method
+    )) %>%
+    mutate(method=forcats::fct_relevel(
+        method, c(method_levels, sprintf("%s (Full)", method_levels))
+    )) %>%
+    ggplot(aes(x=method, y=AUPRC, color=method2)) +
     geom_boxplot() +
     scale_color_manual(values=method_colors) +
+    geom_vline(xintercept=6.5, lty='dotted', linewidth=.5) +
     ylim(0,1) +
     facet_grid(regime~nguides) +
     theme_bw(base_size=16) +
@@ -436,15 +535,43 @@ df_prec_recall_varyNguides %>%
     theme(axis.text.x=element_text(angle=45, hjust=1), legend.position='bottom')
 dev.off()
 
+pdf(file.path(plot_dir, "numCells20k_varyNumGuides_hist_teststats.pdf"),
+    width=14, height=7)
+df_fishash_teststats_varyNguides %>%
+    ggplot(aes(x=test_stat, fill=type)) +
+    geom_histogram(bins=50) +
+    scale_x_continuous(trans='log1p',
+                       breaks=c(0,1,10,100,1000,10000, 100000),
+                       minor_breaks=c(3,30,300,3000,30000)) +
+    facet_grid(regime~nguides, labeller='label_both') +
+    theme_bw(base_size=16) +
+    #scale_fill_manual(values=pals::brewer.paired(4)) +
+    scale_fill_manual(values=pals::okabe(4)) +
+    xlab('-log(p)') +
+    coord_cartesian(ylim=c(0, 2e5)) +
+    theme(legend.position='bottom')
+dev.off()
 
-### TODO: Update plots below!
 
-## varyMoi scenario
+## VARYING MOI SCENARIOS
 
-df_prec_recall_varyMoi <- read.csv(
-    file.path(OUTS, "results", "numCells20k_numGuides200_varyMOI_varyPrecision",
-              "combined_confusion.csv")
+
+# prefix for file paths
+
+prefix_moi <- file.path(
+    OUTS, "results", "numCells20k_numGuides200_varyMOI_varyPrecision"
 )
+
+prefix_moi_orig <- file.path(
+    OUTS, "results", "numCells20k_numGuides200_varyMOI"
+)
+
+# Load the results from discrete parameter sweep
+
+df_prec_recall_varyMoi <- read.csv(file.path(
+    prefix_moi,
+    "combined_confusion.csv"
+))
 
 df_prec_recall_varyMoi %<>% 
     clean_dataframe_discreteSweep()
@@ -453,24 +580,205 @@ df_prec_recall_varyMoi %<>% cbind(parse_simlab_varyMoi(.$sim_label))
 
 df_prec_recall_varyMoi %>%
     group_by(moi) %>%
-    group_by(method, tuning_param, precision_nominal_bound, .add=TRUE) %>%
-    aggregate_prec_recall() ->
-    df_prec_recall_varyMoi_sum
-
-df_prec_recall_varyMoi %>%
-    group_by(moi) %>%
     add_avg_prec_score() ->
     df_prec_recall_varyMoi_auprc
 
+# Load the results from the continuous test statistics
+
+list_teststats_moi <- readRDS(
+    file.path(
+        prefix_moi, "combined_teststats.Rds"
+    )
+)
+
+df_teststats_varyMoi <- lapply(
+    list_teststats_moi,
+    teststats_pair2df
+) %>%
+    rbindlist() %>%
+    as.data.frame()
+
+df_teststats_varyMoi %<>%
+    select(sim_label) %>%
+    distinct() %>%
+    cbind(parse_simlab_varyMoi(.$sim_label)) %>%
+    right_join(df_teststats_varyMoi) %>%
+    mutate(method=if_else(
+        method=='fishash_refit10_padj0.05',
+        'fishash', method
+    ))
+
+# compute AUPRC per replicate
+
+df_teststats_varyMoi %>%
+    lazy_dt() %>%
+    group_by(moi, method, sim_label) %>%
+    add_teststat_cutoff_metrics() %>%
+    summarize(AUPRC=average_precision_score(TN, FN, FP, TP),
+              .groups='drop') %>%
+    as.data.frame() ->
+    df_teststats_varyMoi_auprc
+
+# compute precision-recall for test-stats, aggregating the replicates
+# within each sub-scenario
+df_teststats_varyMoi %>%
+    lazy_dt() %>%
+    group_by(moi, method) %>%
+    add_teststat_cutoff_metrics() %>%
+    ungroup() %>%
+    as.data.frame() ->
+    df_teststats_aggPrecRecall_varyMoi
+
+df_teststats_aggPrecRecall_varyMoi %>%
+    lazy_dt() %>%
+    group_by(moi, method) %>%
+    compress_prc_df(1001) ->
+    df_teststats_aggPrecRecall_varyMoi_compressed
+
+# Load precision-recall from the older run to get the crispat results
+# evaluated at a single point, no precision-recall tradeoff
+
+df_prec_recall_varyMoi_crispat <- read.csv(file.path(
+        prefix_moi_orig,
+        "combined_confusion.csv"
+)) %>%
+    filter(subset=='full') %>%
+    filter(startsWith(method, 'crispat'))
+
+df_prec_recall_varyMoi_crispat %<>% cbind(parse_simlab_varyMoi(.$sim_label))
+
+df_prec_recall_varyMoi_crispat %<>%
+    group_by(moi, method) %>%
+    summarize(TN=sum(TN), FN=sum(FN), FP=sum(FP), TP=sum(TP),
+              .groups='drop') %>%
+    add_precision_recall()
+
+# Read fishash assignments from previous run for log-p-value histogram plot
+
+list_assignments_moi <- c(
+    readRDS(file.path(
+        prefix_moi_orig,
+        "combined_results.Rds"
+    ))
+)
+
+df_fishash_teststats_varyMoi <- merge_fishash_assignments_teststats(
+    list_teststats_moi, list_assignments_moi
+)
+
+df_fishash_teststats_varyMoi %<>%
+    dplyr::select(sim_label) %>%
+    dplyr::distinct() %>%
+    cbind(parse_simlab_varyMoi(.$sim_label)) %>%
+    dplyr::right_join(
+        df_fishash_teststats_varyMoi       
+    )
+
+## Plots
+
+pdf(file.path(plot_dir, "numCells20k_varyMoi_varyPrecision_precVsRecall.pdf"),
+    width=14, height=7)
+bind_rows(
+    df_prec_recall_varyMoi %>%
+        group_by(method, moi, tuning_param, precision_nominal_bound) %>%
+        summarize(TN=sum(TN), FN=sum(FN), FP=sum(FP), TP=sum(TP),
+                  .groups='drop') %>%
+        add_precision_recall() %>%
+        mutate(curve_type="Discretized") %>%
+        mutate(default=discreteSweep_at_default(method, tuning_param)),
+    df_teststats_aggPrecRecall_varyMoi_compressed %>%
+        mutate(curve_type="Full"),
+    df_prec_recall_varyMoi_crispat %>%
+        mutate(curve_type="Discretized",
+               default=TRUE),
+    ) %>%
+    mutate(method=forcats::fct_relevel(method, method_levels)) %>%
+    rename(default_threshold=default) %>%
+    arrange(desc(curve_type), method) %>% {
+        ggplot(., aes(x=Recall, y=Precision, color=method)) +
+            geom_step(aes(lty=curve_type), direction="vh") +
+            geom_point(aes(shape=default_threshold),
+                       data=filter(., curve_type=='Discretized')) +
+            facet_wrap(~moi, nrow=2, labeller='label_both') +
+            scale_color_manual(values=method_colors) +
+            scale_shape_manual(values=c(1,8)) +
+            scale_linetype_manual(values=c('solid', 'dotdash')) +
+            xlim(0,1) +
+            ylim(0,1) +
+            guides(color="none") +
+            theme_classic(base_size=16) +
+            theme(axis.text.x=element_text(angle=45, hjust=1),
+                  legend.position='bottom')
+    }
+dev.off()
+
+pdf(file.path(plot_dir, "numCells20k_varyMoi_varyPrecision_auprc_vs_f1.pdf"),
+    width=14, height=7)
+  set.seed(12345)
+  bind_rows(
+      df_teststats_varyMoi_auprc %>%
+          mutate(curve_type="Full"),
+      df_prec_recall_varyMoi_auprc %>%
+          mutate(curve_type="Discretized")
+  ) %>%
+      full_join(
+          df_prec_recall_varyMoi %>%
+              filter(discreteSweep_at_default(method, tuning_param)) %>%
+              mutate(F1=f1_score(Precision, Recall)) %>%
+              select(method, sim_label, moi, F1)
+      ) %>%
+      .[sample(1:nrow(.)),] %>%
+      arrange(curve_type) %>%
+      ggplot(aes(x=F1, y=AUPRC, color=method, shape=curve_type)) +
+      geom_point(aes(size=curve_type)) +
+      scale_color_manual(values=method_colors) +
+      facet_wrap(~moi, nrow=2, labeller='label_both') +
+      #xlim(0,1) + ylim(0,1) +
+      scale_shape_manual(values=c(1,4)) +
+      scale_size_manual(values=c(3,1.5)) +
+      theme_bw(base_size=16) +
+      theme(legend.position='bottom')
+dev.off()
+
+pdf(file.path(plot_dir, "numCells20k_varyMoi_varyPrecision_auprc.pdf"),
+    width=14, height=7)
+bind_rows(
+    df_teststats_varyMoi_auprc %>%
+        mutate(curve_type="Full"),
+    df_prec_recall_varyMoi_auprc %>%
+        mutate(curve_type="Discretized")
+) %>%
+    mutate(method2=method) %>%
+    mutate(method=if_else(
+        curve_type == 'Full',
+        sprintf("%s (Full)", method),
+        method
+    )) %>%
+    mutate(method=forcats::fct_relevel(
+        method, c(method_levels, sprintf("%s (Full)", method_levels))
+    )) %>%
+    ggplot(aes(x=method, y=AUPRC, color=method2)) +
+    geom_boxplot() +
+    scale_color_manual(values=method_colors) +
+    geom_vline(xintercept=6.5, lty='dotted', linewidth=.5) +
+    ylim(0,1) +
+    facet_wrap(~moi, nrow=2, labeller='label_both', scales='free_x') +
+    theme_bw(base_size=16) +
+    theme(axis.text.x=element_text(angle=45, hjust=1), legend.position='none')
+dev.off()
+
 pdf(file.path(plot_dir, "numCells20k_varyMoi_varyPrecision_precVsNom.pdf"),
     width=14, height=7)
-df_prec_recall_varyMoi_sum %>%
+df_prec_recall_varyMoi %>%
+    group_by(moi) %>%
+    group_by(method, tuning_param, precision_nominal_bound, .add=TRUE) %>%
+    aggregate_prec_recall() %>%
     filter(method != 'demuxem') %>%
-    ggplot(aes(x=precision_nominal_bound, y=Precision_agg, color=method)) +
+    ggplot(aes(x=precision_nominal_bound, y=Precision, color=method)) +
     geom_line() +
     geom_point() +
     geom_abline(lty='dotted') +
-    facet_wrap(~moi, labeller='label_both', nrow=2) +
+    facet_wrap(~moi, nrow=2, labeller='label_both') +
     scale_color_manual(values=method_colors) +
     xlim(0,1) +
     ylim(0,1) +
@@ -480,56 +788,20 @@ df_prec_recall_varyMoi_sum %>%
     theme(axis.text.x=element_text(angle=45, hjust=1), legend.position='bottom')
 dev.off()
 
-pdf(file.path(plot_dir, "numCells20k_varyMoi_varyPrecision_precVsRecall.pdf"),
+pdf(file.path(plot_dir, "numCells20k_varyMoi_hist_teststats.pdf"),
     width=14, height=7)
-df_prec_recall_varyMoi_sum %>%
-    ggplot(aes(y=Precision_agg, x=Recall_agg, color=method)) +
-    geom_step(direction="vh") +
-    geom_point() +
-    facet_wrap(~moi, labeller='label_both', nrow=2) +
-    scale_color_manual(values=method_colors) +
-    xlim(0,1) +
-    ylim(0,1) +
-    xlab("Recall") +
-    ylab("Precision") +
-    coord_flip() +
+df_fishash_teststats_varyMoi %>%
+    ggplot(aes(x=test_stat, fill=type)) +
+    geom_histogram(bins=50) +
+    scale_x_continuous(trans='log1p',
+                       breaks=c(0,1,10,100,1000,10000, 100000),
+                       minor_breaks=c(3,30,300,3000,30000)) +
+    facet_wrap(~moi, nrow=2, labeller='label_both') +
     theme_bw(base_size=16) +
-    theme(axis.text.x=element_text(angle=45, hjust=1), legend.position='bottom')
-dev.off()
-
-pdf(file.path(plot_dir, "numCells20k_varyMoi_varyPrecision_auprc.pdf"),
-    width=14, height=7)
-df_prec_recall_varyMoi_auprc %>%
-    ggplot(aes(x=method, y=auprc, color=method)) +
-    geom_boxplot() +
-    scale_color_manual(values=method_colors) +
-    ylim(0,1) +
-    facet_wrap(~moi, labeller='label_both', nrow=2) +
-    theme_bw(base_size=16) +
-    theme(axis.text.x=element_text(angle=45, hjust=1))
-dev.off()
-
-pdf(file.path(plot_dir, "numCells20k_varyMoi_varyPrecision_auprc_vs_f1.pdf"),
-    width=14, height=7)
-df_prec_recall_varyMoi %>%
-    mutate(
-        default=case_when(
-            method=='demuxem' ~ tuning_param == 2,
-            method=='sceptre_mixture' ~ precision_nominal_bound == 0.8,
-            TRUE ~ precision_nominal_bound == 0.95
-        )
-    ) %>%
-    filter(default) %>%
-    mutate(default_f1=f1_score(Precision, Recall)) %>%
-    select(method, subset, sim_label, moi, default_f1) %>%
-    right_join(df_prec_recall_varyMoi_auprc) %>%
-    ggplot(aes(x=default_f1, y=auprc, color=method)) +
-    geom_point(shape=1) +
-    scale_color_manual(values=method_colors) +
-    facet_wrap(~moi, labeller='label_both', nrow=2) +
-    #xlim(0,1) + ylim(0,1) +
-    xlab("F1 (at default cutoffs)") +
-    ylab("AUPRC (coarse-grained estimate)") +
-    theme_bw(base_size=16) +
+    #scale_fill_manual(values=pals::brewer.paired(4)) +
+    scale_fill_manual(values=pals::okabe(4)) +
+    xlab('-log(p)') +
+    coord_cartesian(ylim=c(0, 2e5)) +
     theme(legend.position='bottom')
 dev.off()
+
