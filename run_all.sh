32c71bcb9bd8e29cceb1752a174d2080951aace9
Jack Kamm <jackkamm@gmail.com>
2026-06-08 20:30:04 +0000

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -17,14 +17,26 @@ sbatch < 23_process_replogle2022.slurm
 # Running these in sequence may take a long time (days).  Consider
 # running each of these in a separate screen/tmux pane in parallel
 ./31_nextflow_run_test.bash
+
 ./34_nextflow_run_numCells20k_medUmi100_snr4_endo75_varyNumGuides.bash
 ./35_nextflow_run_numCells20k_medUmi20_snr1_endo25_varyNumGuides.bash
 ./36_nextflow_run_numCells20k_numGuides200_varyMOI.bash
+
 ./37_nextflow_run_cleanser_barnyard_data.bash
 ./38_nextflow_run_numCells20k_varySignalNoiseCorr.bash
 
 ./39_nextflow_run_replogle2022.bash
 
+./44_nextflow_run_numCells20k_medUmi100_snr4_endo75_varyNumGuides_highPhiNoise.bash
+./45_nextflow_run_numCells20k_medUmi20_snr1_endo25_varyNumGuides_highPhiNoise.bash
+./46_nextflow_run_numCells20k_numGuides200_varyMOI_highPhiNoise.bash
+
+./54_nextflow_run_numCells20k_medUmi100_snr4_endo75_varyNumGuides_varyPrecision.bash
+./55_nextflow_run_numCells20k_medUmi20_snr1_endo25_varyNumGuides_varyPrecision.bash
+./56_nextflow_run_numCells20k_numGuides200_varyMOI_varyPrecision.bash
+
 # Recommended: 4 cpus, 16GB memory.
-Rscript 61_plot_simulation_results.R
-Rscript 62_plot_barnyard_results.R
+Rscript 81_plot_simulation_results.R
+Rscript 82_plot_barnyard_results.R
+Rscript 83_plot_replogle2022_results.R
+Rscript 84_plot_vary_precision.R
